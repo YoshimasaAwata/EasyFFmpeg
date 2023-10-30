dfd3c9e094496528240f6c14b17116ac668a1617
YoshimasaAwata <yoshia.awata@nifty.ne.jp>
2023-10-30 23:17:57 +0900

    ビデオおよびオーディオオプションを追加するウィンドウ とりあえずファイルだけ作成

---
+++ AudioOptionsBox.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyFFmpeg
+{
+    /// <summary>
+    /// AboutBox.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AudioOptionsBox : Window
+    {
+        public AudioOptionsBox()
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        /// <summary>
+        /// ウィンドウをドラッグして移動
+        /// </summary>
+        /// <remarks>
+        /// マウスボタンがリリースされた後にコールされる場合があるのでマウスボタンが押されている事をチェック
+        /// </remarks>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        /// <summary>
+        /// AboutBoxをクローズ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
